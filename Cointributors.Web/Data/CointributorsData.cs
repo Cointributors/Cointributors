ef3a2ff2b2fe215e5aa6a2edbb625b332ed9cf0a
Dylan McKendry <dylan.mckendry@outlook.com>
2024-09-21 13:24:23 +0800

    Updating layout Adding simple dependecy check for requirements.txt

---
+++ Cointributors.Web/Data/CointributorsData.cs
@@ -0,0 +1,22 @@
+﻿namespace Cointributors.Web.Data
+{
+    public enum DestinationType
+    {
+        User,
+        Dependency
+    }
+
+    public class CointributorsData
+    {
+        public string Address { get; set; }
+
+        public List<CointributorsAllocationData> Allocations { get; set; }
+    }
+
+    public class CointributorsAllocationData
+    {
+        public string Address { get; set; }
+        public DestinationType Type { get; set; }
+        public int Allocation { get; set; }
+    }
+}
